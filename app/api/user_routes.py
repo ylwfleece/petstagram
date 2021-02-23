2a035678533175eedfd6bc27cc53a04b959d76e1
Samuel <samuelnunn90@gmail.com>
2021-02-23 16:12:06 -0500

    follow, get all/remove/add work in progress

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from ..app.models import db, Follow, User
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +17,40 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+
+# follow routes ----------
+@user_routes.route('/user/:id/followers')
+@login_required
+def get_all_user_followers():
+    followers = db.session.query(Follow).all()
+    return {"followers": [follower.to_dict() for follower in followers]}
+
+
+@user_routes.route("/:id/follower", methods=["POST"])
+@login_required
+def add_follower(id):
+    # Creates a follow relationship
+    followed_id = Follow.query.get(id)
+    follower_id = request.json["followerId"]
+    if current_user.id != follower_id:
+        return jsonify({"error": "Not authorized"})
+    follower = User.query.get(follower_id)
+    user = User.query.get(id)
+    user.followers.append(follower)
+    db.session.commit()
+    return jsonify({"followed": True})
+
+
+@user_routes.route("/:id/follower", methods=["DELETE"])
+@login_required
+def delete_follower(id):
+    # delete a follow relationship
+    follower_id = request.json["followerId"]
+    if current_user.id != id:
+        return jsonify({"error": "Not authorized"})
+    follower = User.query.get(follower_id)
+    user = User.query.get(id)
+    user.followers.remove(follower)
+    db.session.commit()
+    return jsonify({"unfollowed": True})
