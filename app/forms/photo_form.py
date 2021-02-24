d8b6e689487b6e0b4f5c1d44b16d271672b3c0e8
Samuel <samuelnunn90@gmail.com>
2021-02-24 15:51:06 -0500

    file field photo_form correction

--- app/forms/photo_form.py
+++ app/forms/photo_form.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FileField
+from wtforms import StringField
+from flask_wtf.file import FileField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import Photo
 
