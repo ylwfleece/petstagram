e5d67656acb9bfefe6517f65e42e3de97da604ed
Evanmhargett <evanmhargett@gmail.com>
2021-02-24 12:40:30 -0500

    Added store for session, need remove user and pic

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,21 +1,23 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import commentsReducer from "./comments";
+import sessionReducer from "./session"
 
 const rootReducer = combineReducers({
   comments: commentsReducer,
+  session: sessionReducer, 
 });
 
 let enhancer;
 
-// if (process.env.NODE_ENV === "production") {
-enhancer = applyMiddleware(thunk);
-// } else {
-//   const logger = require("redux-logger").default;
-//   const composeEnhancers =
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-// }
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
 
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
---
+++ react-app/src/store/session.js
@@ -0,0 +1,71 @@
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const addUser = (user) => async (dispatch) => {
+//   const { id, username, email, profilePhotoUrl } = user;
+
+  dispatch(setUser(user));
+  return user;
+};
+
+// export const restoreUser = () => async dispatch => {
+//   const res = await fetch('/api/session');
+//   dispatch(setUser(res.data.user));
+//   return res;
+// };
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
+    case REMOVE_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+// export const signup = (user) => async (dispatch) => {
+//   const { username, email, password } = user;
+//   const response = await fetch("/api/users", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       username,
+//       email,
+//       password,
+//     }),
+//   });
+//   dispatch(setUser(response.data.user));
+//   return response;
+// };
+
+// export const logout = () => async (dispatch) => {
+//   const response = await fetch('/api/session', {
+//     method: 'DELETE',
+//   });
+//   dispatch(removeUser());
+//   return response;
+// };
+
+export default sessionReducer;
