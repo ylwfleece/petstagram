b6e167900db671aec5599ef49b6006ad1acdfda4
Samuel <samuelnunn90@gmail.com>
2021-02-24 14:08:32 -0500

    like routes done

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ from .api.auth_routes import auth_routes
 from .api.comment_routes import comment_routes
 # from .api.follow_routes import follow_routes
 from .api.post_routes import post_routes
+from .api.like_routes import like_routes
 
 from .seeds import seed_commands
 
@@ -37,6 +38,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(comment_routes, url_prefix='/api/comments')
 # app.register_blueprint(follow_routes, url_prefix='/api/follows')
 app.register_blueprint(post_routes, url_prefix='/api/posts')
+app.register_blueprint(like_routes, url_prefix='/api/likes')
 db.init_app(app)
 Migrate(app, db)
 
