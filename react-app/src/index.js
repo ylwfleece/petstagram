a824580d292b0ff66322551c6b132fc83e2485fe
River <cosmos.cha1997@gmail.com>
2021-02-23 23:36:12 -0500

    created skeleton for frontend component for comment page

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+//redux stuff
+import configureStore from "./store";
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+//end redux
+function Root() {
+  return (
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
