979309c97d3f2543b138b54031a39f347363c1b1
Samuel <samuelnunn90@gmail.com>
2021-02-24 16:43:56 -0500

    updating routes

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -4,10 +4,30 @@ from werkzeug.utils import secure_filename
 from ..helpers import *
 from ..config import Config
 from app.models import Post, User, db
-from app.forms import PhotoForm
+# from app.forms import PhotoForm
 
 post_routes = Blueprint('posts', __name__)
 
+
+@post_routes.route('/<userId>', methods=["GET"])
+# @login_required
+def get_posts(userId):
+    user = User.query.get(userId)
+    followers = user.followers # people the user follows
+    # follows = user.follows (people who follow the user)
+    ids_to_query = [int(userId)]
+    for follower in followers:
+        ids_to_query.append(follower.id)
+    # print(ids_to_query)
+    posts = set()
+    for id in ids_to_query:
+        posts_from_userId = Post.query.filter(Post.userId == id).all()
+        posts = posts | set(posts_from_userId)
+    # print('===> posts: ', posts)
+    posts_to_return = []
+    for post in posts:
+        posts_to_return.append(post.to_dict())
+    return jsonify(posts_to_return)
 
 @post_routes.route('/', methods=["POST"])
 @login_required
@@ -35,4 +55,3 @@ def create_posts():
         db.session.commit()
         return jsonify(post.to_dict())
     return {'errors': validation_errors_to_error_messages(form.errors)}
-
