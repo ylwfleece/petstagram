2a8278f479db52948d57226356a83872d396614c
River <cosmos.cha1997@gmail.com>
2021-02-23 22:18:19 -0500

    comments route

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
-from app.models import Comment
+from flask import Blueprint, jsonify
+from flask_login import login_required, current_user
+from ..helpers import *
+from ..config import Config
+from app.models import Comment, Post
 from app.forms import CommentForm
 
 
@@ -7,7 +10,46 @@ comment_routes = Blueprint('comments', __name__)
 
 
 @comment_routes.route('/')
-def makeComment():
-    comments = Comment.query.all()
-    print("comments", comments)
-    return {"Comments": [comment.to_dict() for comment in comments]}
+def getPostComments(id):
+    post = Post.query.get(id)
+    comments = Comment.query.filter_by(postId=post.id).all()
+
+    def comment_info(self):
+        return {
+            "id": self.id,
+            "postId": self.postId,
+            "userId": self.userId,
+            "content": self.content
+        }
+    return jsonify({"comments": [comment_info(comment) for comment in comments]})
+
+
+@comment_routes.route('/', methods=["POST"])
+@login_required
+def createComment(id):
+    post = Post.query.get(id)
+    form = CommentForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        commentData = form.data
+
+        comment = Comment(
+            postId=post.id,
+            userId=current_user.get_id(),
+            content=commentData
+        )
+
+        db.session.add(comment)
+        db.session.commit()
+        return jsonify(comment.to_dict())
+    return "Bad Data"
+
+# in progress delete route
+# @comment_routes.route('/', methods=["POST"])
+# @login_required
+# def deleteComment(id):
+#     comment = Post.query.options(lazyload('comments')).first()
+#     db.session.remove(comment)
+#     db.session.commit
+#     return
