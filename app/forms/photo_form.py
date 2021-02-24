cec6bf4f742a69fc66f5bdbf3a567e7bb726052b
Samuel <samuelnunn90@gmail.com>
2021-02-24 12:09:40 -0500

    Fixing bugs

--- app/forms/photo_form.py
+++ app/forms/photo_form.py
@@ -1,8 +1,9 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField
-# from wtforms.validators import DataRequired, Email, ValidationError
-# from app.models import Photo
+from flask_wtf import FlaskForm
+from wtforms import StringField, FileField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Photo
 
 
-
-# class PhotoForm(FlaskForm):
+class PhotoForm(FlaskForm):
+    feed_photo_file = FileField('profile photo',  validators=[DataRequired()]),
+    caption = StringField('caption')
