e2394cba866446703dab58e0cb0e86a0d710be22
Maxwell Lettenberger <maxwelllettenberger@Maxwells-MacBook-Air.local>
2021-02-24 15:29:26 -0600

    Now returning valid object from post route

--- react-app/src/store/posts.js
+++ react-app/src/store/posts.js
@@ -1,1 +1,34 @@
-const GET_POSTS = "posts/getPosts"
+const SET_POSTS = "posts/setPosts"
+
+const setPosts = (posts) => {
+    return {
+      type: SET_POSTS,
+      payload: posts
+    };
+  };
+
+export const getPostsForUser = (userId) => async (dispatch) => {
+    const posts = await fetch(`/api/posts/${userId}`);
+    dispatch(setPosts(posts));
+    return posts;
+  };
+
+const initialState = { posts: null };
+
+const postsReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_POSTS:
+      newState = Object.assign({}, state);
+      newState.posts = action.payload;
+      return newState;
+    case REMOVE_POSTS:
+      newState = Object.assign({}, state);
+      newState.posts = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default postsReducer;
