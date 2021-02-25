df545ac4a5a938c9e3d4534e7f58057ce0cf667b
Maxwell Lettenberger <maxwelllettenberger@Maxwells-MacBook-Air.local>
2021-02-25 14:43:12 -0600

    Search results displaying

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -2,37 +2,29 @@ import React, { useState } from "react";
 import { Redirect, useHistory } from 'react-router-dom';
 // import './SignUpForm.css'
 import petstagramlogo from '../NavBar/petstagramlogo.png'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 // import {createPost} from '../../store/posts'
 
 const SearchResults = ({ authenticated, setAuthenticated }) => {
-//   const [caption, setCaption] = useState("");
-//   const [photoFile, setPhotoFile] = useState("");
-//   const dispatch = useDispatch();
-//   const history = useHistory();
 
-//   const onPost = async (e) => {
-//     e.preventDefault();
-//     const post = dispatch(createPost(caption, photoFile));
-//     history.push('/')
-//   };
-
-//   const updateCaption = (e) => {
-//     setCaption(e.target.value);
-//   };
-
-//   const updatePhotoFile = (e) => {
-//     setPhotoFile(e.target.files[0]);
-//   }
-
-
-
-
+    const searchResults = useSelector(state => state.search);
   return (
     <div className='page-container'>
       <div>
-        in search results: 
+        search results: 
       </div>
+      {searchResults && searchResults.map(user => (
+          <div key={user.id} className="search-result container">
+              {/* <Link to={`/project/${project.id}`}> */}
+            <p>
+                {user.username}
+            </p>
+            
+              {/* </Link> */}
+              {/* <p key={project.costPerShare}>cost per share: ${project.costPerShare} {(!(purchasedProjects.includes(project.id) || watchlistedProjects.includes(project.id))) && <button onClick={(e) => addToWatchlist(e, project.id)}>add to watchlist</button>}</p> 
+              <p key={project.id} className="success" hidden={(!success || watchProjectId !== project.id)}>successfully added to watchlist</p>  */}
+          </div>
+        ))}
     </div>
   );
 };
