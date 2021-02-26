66d85764d4d819970b74eb69f2a5f1e86defaa33
Sebastian Tan <sebthtan@yahoo.com>
2021-02-26 12:47:11 -0600

    almost done posts page

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import './HomePage.css'
 
 function HomePage() {
@@ -23,11 +24,18 @@ function HomePage() {
             <div className='sidebar'>
                 <div className='user-display flex-left-container'>
                     <div className='rounded-img-container'>
-                        <img src={user.profilePhotoUrl} alt='profilepic' />
+                        <Link to={`/users/${user.id}`}>
+                            <img src={user.profilePhotoUrl} alt='profilepic' />
+                        </Link>
                     </div>
                     <div>
-                        <h5 className='normalize-text' style={{ color: 'rgb(38, 38, 38)', fontWeight: '600' }}>{user.username}</h5>
-                        <p className='normalize-text'>{user.email}</p>
+                        <Link to={`/users/${user.id}`}>
+                            <h5 className='normalize-text redirect-profile' style={{ color: 'rgb(38, 38, 38)', fontWeight: '600' }}>{user.username}</h5>
+                        </Link>
+                        <div>
+
+                            <p className='normalize-text'>{user.email}</p>
+                        </div>
                     </div>
                 </div>
             </div>
