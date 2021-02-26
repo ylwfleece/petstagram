81ba513dfe83d0b100dc5a62a2df0d9adea0a609
Samuel <samuelnunn90@gmail.com>
2021-02-26 11:11:36 -0500

    Pictures working

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -12,7 +12,8 @@ const customStyles = {
       right                 : 'auto',
       bottom                : 'auto',
       marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
+      transform             : 'translate(-50%, -50%)',
+      
     }
   };
 
@@ -21,12 +22,24 @@ Modal.setAppElement('body');
 const UserProfile = () => {
     const dispatch = useDispatch();
     const sessionUserId = useSelector(state => state.session.user.id);
+    const sessionUserName = useSelector(state => state.session.user.username);
+    const sessionProfilePhoto = useSelector(state => state.session.user.profilePhotoUrl);
     const userPosts = useSelector(state => state.posts);
+    const captionMap = userPosts.map(post => {
+        return post.caption
+    })
+
+    const helperFunction = (captionMap) => {
+
+    }
+
     const [modalIsOpen, setIsOpen] = useState(false);
     const [targetPhoto, setTargetPhoto] = useState([])
-
+  
 
     const openModal = (image) => {
+        console.log(image.target.alt.caption)
+        
         setTargetPhoto(image.target.src)
         setIsOpen(true);
     };
@@ -34,7 +47,6 @@ const UserProfile = () => {
     const closeModal = () => {
         setIsOpen(false);
     };
-
 
     const userImageLinks = userPosts.map(eachPicture => {
         return eachPicture.imageLinks;
@@ -44,8 +56,7 @@ const UserProfile = () => {
     });
 
     const userImageId = userImageIdMap[0];
-    
-    
+
     return (
         <>
         <div className='center-me'>
@@ -56,14 +67,23 @@ const UserProfile = () => {
                         return (
                             <>
                                 <div className='each-post'>
-                                    <img src={image} className='each-picture' onClick={openModal}></img>
+                                    <img src={image} alt={userPosts} className='each-picture' onClick={openModal}></img>
                                     <Modal 
                                         isOpen={modalIsOpen}
                                         onRequestClose={closeModal}
                                         style={customStyles}
                                         contentLabel="userPictureModal"
                                         >
-                                        <img src={targetPhoto} className='each-post'></img>
+                                            <div className='comments-modal'>
+                                                <img src={targetPhoto} className='modal-picture'></img>
+                                                <div className='username-field'>
+                                                    <img src={sessionProfilePhoto} className='profile-photo'/>
+                                                    <h4 className='username-text'>{sessionUserName}</h4>
+                                                </div>
+                                                <p className='caption-field'>{captionMap[0]}</p>
+                                                <p className='like-message-field'>I am likes/messages</p>
+                                                <p className='add-comment-field'>I am comments</p>
+                                            </div>
                                     </Modal>
                                 </div>
                             </>
