7eb26108ff75a488c552dc04b60c84f33833c28d
Samuel <samuelnunn90@gmail.com>
2021-02-25 19:04:56 -0500

    Modal needs work

--- react-app/src/components/Modal/comment-modal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CommentModal = () => {
-    return (
-        <h1>Hello!</h1>
-    )
-}
-
-export default CommentModal;
--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -1,55 +1,90 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Redirect, useHistory } from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux'
-import CommentModal  from '../Modal/comment-modal'
-import './userProfile.css'
+import {useDispatch, useSelector} from 'react-redux';
+import Modal from 'react-modal';
+import './userProfile.css';
+
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
+
+Modal.setAppElement('body');
 
 const UserProfile = () => {
     const dispatch = useDispatch();
     const sessionUserId = useSelector(state => state.session.user.id);
     const userPosts = useSelector(state => state.posts);
-    const [ modalOpen, setModalOpen] = useState(false)
+    const [modalIsOpen, setIsOpen] = useState(false);
+    
 
-    const showModal = () => {
-        setModalOpen(true)
+    const openModal = () => {
+        setIsOpen(true);
     };
 
-    const hideModal = () => {
-        setModalOpen(false);
+    const closeModal = () => {
+        setIsOpen(false);
     };
-    
-    
+
+    useEffect(() => {
+        const onClick = (event) => {
+            console.log(event.target)
+        }
+    },[Modal])
+
+
     const userImageLinks = userPosts.map(eachPicture => {
-        return eachPicture.imageLinks
+        return eachPicture.imageLinks;
     });
     const userImageIdMap = userPosts.map(eachPicture => {
-        return eachPicture.userId
+        return eachPicture.userId;
     });
 
     const userImageId = userImageIdMap[0];
-    
+    const myImageLink = userImageLinks[0];
+    console.log(myImageLink);
+    const myImages = userImageLinks;
 
+    // images.map(image => <div><Modal><img src={image.url}/> </Modal></div>)
 
     return (
         <>
         <div className='center-me'>
             <div className='post-container'>
                 {!userImageLinks && <h3>You have no posts yet!</h3>}
-                {userImageLinks && userImageLinks.map(images => {
+                {userImageLinks && userImageLinks.map(image => {
                     if (sessionUserId === userImageId) {
+                        let theImageINeed;
                         return (
                             <>
                                 <div className='each-post'>
-                                    <img src={images} className='each-picture'></img>
+                                    <img src={image} className='each-picture' onClick={openModal}></img>
+                                    <Modal 
+                                        isOpen={modalIsOpen}
+                                        onRequestClose={closeModal}
+                                        style={customStyles}
+                                        contentLabel="userPictureModal"
+                                        theImageINeed={image}
+                                        >
+                                            {console.log(image)}
+                                        <img src={theImageINeed} className='each-post'>{console.log(myImages)}</img>
+                                    </Modal>
                                 </div>
                             </>
                         )
                     }   
                 })}
+
             </div>
             </div>
         </>
-    )
+    );
 }
 
 export default UserProfile;
